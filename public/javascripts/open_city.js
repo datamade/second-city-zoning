212e9d234d7575ed961a2c93cdec96da29b6eb89
Chad W Pry <chad.pry@gmail.com>
2012-05-07 01:57:00 -0500

    add filter predicate and filter panel

--- public/javascripts/open_city.js
+++ public/javascripts/open_city.js
@@ -1,84 +1,118 @@
 var OpenCity = OpenCity || {};
+OpenCity.Fusion = OpenCity.Fusion || {};
 OpenCity.Template = OpenCity.Template || {};
+OpenCity.Template.Table = OpenCity.Template.Table || {};
 
-OpenCity.Template.Filter = function(tableId, column, options) {
-  this.tableId = tableId;
-  this.column  = column;
+OpenCity.Fusion.url = "https://www.google.com/fusiontables/api/query";
 
-  if (options["map"] == null) {
-    throw "Map must be provided!";
-  };
+OpenCity.Template.FilterPredicate = function(column, type) {
+  this.column = column;
+  this.type   = "checkbox";
+};
+OpenCity.Template.FilterPredicate.prototype.elements = [];
+OpenCity.Template.FilterPredicate.prototype.sql = function() {
+  var elementArray = this.elements.map(function(index, element) {
+    if (element.checked) {
+      return "'" + element.value + "'";
+    }
+  }).get();
+  return (elementArray.length == 0) ? "" : this.column + " IN (" + elementArray.join(",") + ")";
+};
 
-  OpenCity.Template.Filter.selector = $.extend({
-    selector: "#filter"
+OpenCity.Template.FilterPanel = function(tableId, columns, options) {
+  if (options["map"] == null) {throw "Map must be provided!";}
+  if (options["callbackObject"] == null) {throw "Callback must be provided!";}
+
+  this.map            = options["map"];
+  this.callbackObject = options["callbackObject"];
+  this.tableId        = tableId;
+  this.predicates     = [];
+  var predicates      = this.predicates;
+
+  $.each(columns, function(index, column) {
+    predicates.push(new OpenCity.Template.FilterPredicate(column));
+  });
+
+  this.options = $.extend({
+    selector: "#filters", callbackObject: "filterPanel"
   }, options);
 };
-OpenCity.Template.Filter.options = {selector: "#filters"};
-OpenCity.Template.Filter.parseFilter = function(json) {
+OpenCity.Template.FilterPanel.prototype.parseFilterPanel = function(json) {
   var data = {};
 
   data["column_title"] = json["table"]["cols"][0];
   data["column_rows"] = [];
 
   $.each(json["table"]["rows"], function(index, element) {
-    data["column_rows"].push({"value": element[0]});
+    if (element[0].trim().length > 0) {
+      data["column_rows"].push({"value": element[0]});
+    }
   });
 
   var mustache = Mustache.render(OpenCity.Template.Mustache["filter"], data);
-  $(OpenCity.Template.Filter.options["selector"]).prepend(mustache);
+  $(this.options["selector"]).prepend(mustache);
 
-  OpenCity.Template.Filter.applyEvents(data);
+  this.applyEvents(data);
 };
-OpenCity.Template.Filter.prototype.present = function() {
-  var uri = "https://fusiontables.googleusercontent.com/fusiontables/api/query"
+OpenCity.Template.FilterPanel.prototype.present = function() {
+  var tableId     = this.tableId;
+  var callback    = this.options["callbackObject"];
 
-  var querystring = {};
-  var keys = [];
-  var pairs = [];
+  $.each(this.predicates, function(index, predicate) {
+    var querystring = {};
+    var keys        = [];
+    var pairs       = [];
 
-  querystring["sql"] = "SELECT+"+this.column+",COUNT()+FROM+"+this.tableId+"+GROUP+BY+"+this.column;
-  querystring["jsonCallback"] = "OpenCity.Template.Filter.parseFilter";
+    querystring["sql"] = "SELECT+"+predicate.column+",COUNT()+FROM+"+tableId+"+GROUP+BY+"+predicate.column;
+    querystring["jsonCallback"] = callback+".parseFilterPanel";
 
-  for (var i in querystring) if (querystring.hasOwnProperty(i)) {
-    keys.push(i);
-  };
+    for (var i in querystring) if (querystring.hasOwnProperty(i)) {
+      keys.push(i);
+    };
 
-  $.each(keys, function(index, key) {
-    pairs.push(key+"="+querystring[key]);
+    $.each(keys, function(index, key) {
+      pairs.push(key+"="+querystring[key]);
+    });
+
+    $.ajax({url: OpenCity.Fusion.url+"?"+pairs.join("&"), dataType: "jsonp"});
   });
-
-  $.ajax({url: uri+"?"+pairs.join("&"), dataType: 'jsonp'});
 };
-OpenCity.Template.Filter.applyEvents = function(data) {
-  var data_inputs = $("." + data["column_title"] + " .controls input");
-  data_inputs.click(function() {
-    var where_predicate = data_inputs.map(function(index, element) {
-      if (element.checked) {
-        return data["column_title"]+" = '" + element.value + "'";
-      }
-    }).get();
+OpenCity.Template.FilterPanel.prototype.wherePredicate = function() {
+  return $.map(this.predicates, function(predicate, index) {
+    var sql = predicate.sql();
 
-    if (where_predicate.length == 0) {
-      where_predicate.push(data["column_title"]+" = 'not-earth'");
+    if (sql.trim().length > 0) {
+      return sql;
     }
+  }).join(" AND ");
+};
+OpenCity.Template.FilterPanel.prototype.applyEvents = function(data) {
+  var filter = this;
 
-    map.layers[0].setOptions({
-      query: {select: "latitude", from: "3793626", where: where_predicate.join(" or ")}
+  $.each(this.predicates, function(index, predicate) {
+    predicate.elements = $(".filter ." + predicate.column + " .controls input");
+
+    predicate.elements.click(function() {
+      filter.map.layers[0].setOptions({
+        query: {select: "Latitude", from: filter.tableId, where: filter.wherePredicate()}
+      });
     });
   });
 }
 
 OpenCity.Template.Mustache = OpenCity.Template.Mustache || {};
 OpenCity.Template.Mustache["filter"] = "\
-<h2>{{column_title}}</h2>\
-<div class=\"control-group {{column_title}}\">\
-  <div class=\"controls\">\
-    {{#column_rows}}\
-    <label class=\"checkbox inline\">\
-      <input type=\"checkbox\" checked=\"checked\" value=\"{{value}}\"/>\
-      {{value}}\
-    </label>\
-    {{/column_rows}}\
+<div class=\"filter\">\
+  <h2>{{column_title}}</h2>\
+  <div class=\"control-group {{column_title}}\">\
+    <div class=\"controls\">\
+      {{#column_rows}}\
+      <label class=\"checkbox inline\">\
+        <input type=\"checkbox\" value=\"{{value}}\"/>\
+        {{value}}\
+      </label>\
+      {{/column_rows}}\
+    </div>\
   </div>\
 </div>\
 ";
