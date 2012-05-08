872b4507a245014ae674001bff5c8d3853632d29
Chad W Pry <chad.pry@gmail.com>
2012-05-08 02:31:01 -0500

    add contain ignore case clause

--- public/javascripts/open_city.js
+++ public/javascripts/open_city.js
@@ -16,7 +16,7 @@ OpenCity.Template.FilterControl = function(column, inputType) {
             "" : control.column + " IN (" + elementArray.join(",") + ")";
   };
   var like_clause = function(element) {
-    return ("state LIKE '" + element.val() + "%'");
+    return ("state CONTAINS IGNORING CASE '" + element.val() + "'");
   }
 
   this.sql_functions = {};
