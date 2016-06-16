41e800c1e04d38c6486f0bb5facf3de8b4b39fbe
Derek Eder <derek.eder@gmail.com>
2016-06-15 23:37:53 -0500

    updated cartodb viz url

--- js/cartodb_lib.js
+++ js/cartodb_lib.js
@@ -6,7 +6,7 @@ var CartoDbLib = {
   lastClickedLayer: null,
   locationScope:   "chicago",
   currentPinpoint: null,
-  layerUrl: 'https://datamade.cartodb.com/api/v2/viz/7d3b444e-b35b-11e5-b55f-0ea31932ec1d/viz.json',
+  layerUrl: 'https://datamade.cartodb.com/api/v2/viz/1362b06a-3345-11e6-9b44-0ef7f98ade21/viz.json',
   tableName: 'city_of_chicago_zoning_2015',
 
   initialize: function(){
